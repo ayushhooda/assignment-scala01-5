d8c383406c7cc384b1019a66f5e57da940b32417
Ayush Hooda <ayush.hooda@knoldus.in>
2018-01-15 10:12:37 +0530

    Final submission

--- src/main/scala/CollatzEquation.scala
+++ src/main/scala/CollatzEquation.scala
@@ -1,14 +1,14 @@
 class CollatzEquation {
 
-  def contains(list: List[Int], element: Int): Boolean = {
+  def contain(list: List[Int], element: Int): Boolean = {
     list match {
-      case head :: tail => if(head == element) true else contains(tail, element)
+      case head :: tail => if(head == element) true else contain(tail, element)
       case Nil => false
     }
   }
 
   def check(list: List[Int], element: Int): List[Int] = {
-    if(!list.contains(element)) {
+    if(!contain(list, element)) {
       element match {
         case 1 => list
         case element => if(element % 2 == 0) check(element :: list, element / 2) else check(element :: list, 3*element + 1)
